b98fbec1727676a4ea9c49600ba0bd1d4f959c8f
Rohan Raj <rohanraj7316@gmail.com>
2023-04-27 07:26:34 +0530

    feat: add req res logging

--- interceptors/logging/constant.go
+++
@@ -1,4 +0,0 @@
-package logging
-
-const REQ_RES_RECV_MSG_FORMAT = "status=${status}#method=${method}#path=${path}#latency=${latency}#reqHeaders=${reqHeaders}#requestId=${respHeader:X-Request-Id}#reqBody=${body}#resBody=${resBody}#error=${error}"
-const REQ_RES_LOG_MSG = "[REQ-RES-LOG] %s %s %s %s" // [REQ-RES-LOG] 200 POST /health 2.3sec
--- interceptors/logging/interceptors.go
+++ interceptors/logging/interceptors.go
@@ -2,15 +2,41 @@ package logging
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
+// UnaryServerInterceptor used for logging request and response
 func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler) (resp interface{}, err error) {
-		// start := time.Now()
+		logger, start := log.Info(), time.Now()
+
 		resp, err = handler(ctx, req)
+		if err != nil {
+			logger = log.Error()
+		}
+
+		statusCode := codes.Unknown
+		if st, ok := status.FromError(err); ok {
+			statusCode = st.Code()
+		}
+
+		latency := time.Since(start)
+		logger.Str("protocol", "grpc").
+			Str("method", info.FullMethod).
+			Int("statusCode", int(statusCode)).
+			Str("status", statusCode.String()).
+			Dur("latency", latency).
+			Interface("reqBody", req).
+			Interface("resBody", resp).
+			Msg(fmt.Sprintf("[REQ-RES-LOG] %d %s %s", statusCode, info.FullMethod, latency)) // [REQ-RES-LOG] 0 /health 2.3sec
+
 		return resp, err
 	}
 }

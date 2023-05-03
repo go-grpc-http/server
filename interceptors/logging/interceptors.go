57c75489f7c82e88927c3b4cd412dc2133246f25
Rohan Raj <rohanraj7316@gmail.com>
2023-05-03 19:27:48 +0530

    fix: made str all key

--- interceptors/logging/interceptors.go
+++ interceptors/logging/interceptors.go
@@ -2,9 +2,10 @@ package logging
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"time"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -18,24 +19,44 @@ func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 		logger, start := log.Info(), time.Now()
 
 		resp, err = handler(ctx, req)
-		if err != nil {
-			logger = log.Error()
-		}
 
-		statusCode := codes.Unknown
-		if st, ok := status.FromError(err); ok {
-			statusCode = st.Code()
-		}
+		go func(info *grpc.UnaryServerInfo, req, resp interface{}, start time.Time, logger *zerolog.Event, err error) {
+			if err != nil {
+				logger = log.Error().Err(err)
+			}
 
-		latency := time.Since(start)
-		logger.Str("protocol", "grpc").
-			Str("method", info.FullMethod).
-			Int("statusCode", int(statusCode)).
-			Str("status", statusCode.String()).
-			Dur("latency", latency).
-			Interface("reqBody", req).
-			Interface("resBody", resp).
-			Msg(fmt.Sprintf("[REQ-RES-LOG] %d %s %s", statusCode, info.FullMethod, latency)) // [REQ-RES-LOG] 0 /health 2.3sec
+			statusCode := codes.Unknown
+			if st, ok := status.FromError(err); ok {
+				statusCode = st.Code()
+			}
+
+			latency := time.Since(start)
+
+			if req != nil {
+				reqByte, err := json.Marshal(req)
+				if err != nil {
+					logger.Err(err)
+				}
+
+				logger.Str("reqBody", string(reqByte))
+			}
+
+			if resp != nil {
+				respByte, err := json.Marshal(resp)
+				if err != nil {
+					logger.Err(err)
+				}
+
+				logger.Str("resBody", string(respByte))
+			}
+
+			logger.Str("protocol", "grpc").
+				Str("method", info.FullMethod).
+				Int("statusCode", int(statusCode)).
+				Str("status", statusCode.String()).
+				Dur("latency", latency).
+				Msgf("[REQ-RES-LOG] %d %s %s", statusCode, info.FullMethod, latency) // [REQ-RES-LOG] 0 /health 2.3sec
+		}(info, req, resp, start, logger, err)
 
 		return resp, err
 	}
